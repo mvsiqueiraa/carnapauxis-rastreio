a463a9f2e8ca709a84e251c1473af8ad56b75a63
Marcos Vinicius (mv) <mv.dev212@gmail.com>
2026-02-11 14:42:44 -0300

    Atualizando tile

--- src/App.jsx
+++ src/App.jsx
@@ -188,7 +188,10 @@ export default function App() {
 
       {/* Mapa */}
       <MapContainer center={blocoAtual.inicio} zoom={15} zoomControl={false} className="mapa-layout">
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+/>
         <FlyToLocation center={blocoAtual.inicio} />
 
         {/* Marcador Saída */}
